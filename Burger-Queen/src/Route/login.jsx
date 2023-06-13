f04bee371e5ed2b4bb4445734d025044d1ef89b1
Carolina Vera <cvera.nahuelcoy@gmail.com>
2023-06-13 19:52:08 -0400

    Se finaliza la validacion y se accede a la APi con axios

--- Burger-Queen/src/Route/login.jsx
+++ Burger-Queen/src/Route/login.jsx
@@ -1,16 +1,67 @@
 import '../App.css'
-
 import {Link} from "react-router-dom"
+import  axios  from  'axios' ;
 import { Input  } from '../components/Input'
 import LabelText  from '../components/Label'
 import { Button } from '../components/Button'
-import { Formik, Form, ErrorMessage } from 'formik';
+import { Formik, Form } from 'formik';
 import * as Yup from 'yup';
 
-const validationSchema = Yup.object({
-  email: Yup.string().email('El correo electrónico no es válido').required('El correo electrónico es obligatorio'),
-  password: Yup.string().required('La contraseña es obligatoria'),
-});
+function Login() {
+  const handleSubmit = async (values, { resetForm }) => {
+    const { email, password } = values;
+    try {
+      const response = await axios.post('http://localhost:8080/login', {
+         email,
+         password,
+      });
+
+        const accessToken = response.data.accessToken;
+        console.log(accessToken);
+        resetForm();
+    } catch (error) {
+      if (error.response) {
+        if (error.response.status === 400) {
+          console.error('Error: Se requiere correo y contraseña');
+        } else if (error.response.status === 404) {
+          console.error('Error: Credenciales incorrectas');
+        }
+      } else {
+        console.error('Error: No se pudo conectar al servidor');
+      }
+    }
+  };
+
+  const validationSchema = Yup.object().shape({
+    email: Yup.string().email('Correo electrónico inválido').required('Campo requerido'),
+    password: Yup.string().required('Campo requerido'),
+  });
+
+  return (
+    <>
+      <h1>Burger Queen</h1>
+      <section className="form">
+        <h2>Inicio de Sesión</h2>
+        <Formik
+          initialValues={{ email: '', password: '' }}
+          validationSchema={validationSchema}
+          onSubmit={handleSubmit}
+        >
+          <Form>
+          <LabelText text="Correo Electrónico" />
+              <Input type="email" name="email" />
+              <LabelText text="Contraseña" />
+              <Input type="password" name="password" />
+      <Button type="submit" text='Ingresar' />
+           
+            <Link to='/waiter'></Link> 
+          </Form>
+        </Formik>
+      </section>
+    </>
+  );
+}
+export default Login
 
 /* function Login() {
   const handleSubmit = (values) => {
@@ -42,50 +93,3 @@ const validationSchema = Yup.object({
     </>
   );
 } */
-
-function Login() {
-   const [pass, passInput] = Input ({ type: 'password'})
-  const [email, emailInput] = Input ({ type: 'email'}) 
-  const handleSubmit = (values) => {
-    // Lógica para enviar los datos del formulario
-    console.log(values);
-  };
-
-  return (
-    <>
-    <h1>Burger Queen</h1><section className="form">
-      <h2>Inicio de Sesión</h2>
-      <Formik
-        initialValues={{ email: '', password: '' }}
-        validationSchema={validationSchema}
-        onSubmit={handleSubmit}
-      >
-  
-      <Form>
-      {/* <CustomField label="Correo Electrónico" type="email" name="email" />
-      <CustomField label="Contraseña" type="password" name="password" /> */}
-  
-        <div>
-      <LabelText text="Correo Electrónico" />
-      {emailInput}
-      <ErrorMessage name='email' component='div' />
-        {console.log({email})}
-
-    </div>
-    <div>
-      <LabelText text="Contraseña" />
-      {passInput}
-      <ErrorMessage name='password' component='div' />
-        {console.log({pass})}
-    </div> 
-    <Button onClick={onclick} text='Ingresar'></Button>
-       <Link to='/waiter'>
-        </Link> 
-      </Form>
-      </Formik>
-    </section></>
-
-  )
-      }
-  
-export default Login
--- Burger-Queen/src/components/Input.jsx
+++ Burger-Queen/src/components/Input.jsx
@@ -1,12 +1,22 @@
 /* eslint-disable react/prop-types */
-import { useState } from 'react';
+// import { useState } from 'react';
+import { Field, ErrorMessage } from 'formik';
 
-export const Input = ({type}) => {
+export const Input = ({ type, name }) => {
+  return (
+    <div>
+      <Field type={type} id={name} name={name} />
+      <ErrorMessage name={name} component="div" className="error-message" />
+    </div>
+  );
+}
+
+/* export const Input = ({type}) => {
   const [value, setvalue] = useState('')
   const input = <input value= {value} onChange= {e => setvalue(e.target.value)} type={type}/>
   return [value, input];
 }
-
+ */
 
 /* import { Field, ErrorMessage } from 'formik';
 
