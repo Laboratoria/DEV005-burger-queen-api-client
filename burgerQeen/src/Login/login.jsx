9cb83171afe76ec5d5de1ee544310e182cb151a5
YahairaPerez1994 <yahairaperez2694@gmail.com>
2023-06-13 11:40:10 -0500

    Actualización

--- burgerQeen/src/Login/login.jsx
+++ burgerQeen/src/Login/login.jsx
@@ -8,7 +8,7 @@
 
        
      <div className="inputs">
-      <h1>Bienvenidos</h1>
+      <h1>Bienvenidoss</h1>
      Usuario:<input className="us" type="email" placeholder="ejemplo@gmail.com" required/>
      Contraseña: <input className="pas" type="password" placeholder='*********' required/>      
       <button className="button-form">Ingresar</button>
