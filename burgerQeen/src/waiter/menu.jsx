f1fc46cd7ca45037afdc8542d82a7939766b0d3a
lauralejandra108 <118205850+lauralejandra108@users.noreply.github.com>
2023-06-14 08:08:25 -0500

    avance de la vista de menu

--- burgerQeen/src/waiter/menu.jsx
+++ burgerQeen/src/waiter/menu.jsx
@@ -1,10 +1,24 @@
 import {Link} from "react-router-dom"
+import './menu.css'
 export default function Menu() {
     return ( 
-    <><><>
-<button className="button-form">Enviar a cocina</button>
-        </></><Link to='/Client'
-        ><button>Atras</button>
-            </Link></>
+    <>
+    <div className="grid-container">
+    <h1>Menú</h1>
+    <h2>Desayuno</h2>
+    <h3>Almuerzo</h3>
+    <button className="botton-cook">Enviar a cocina</button>
+        <Link to='/Client'>
+            <button className="botton-back">Atrás</button>
+            </Link>
+            <p>Resumen de orden</p>
+            <div className="order"></div>
+           
+            <button className="coffee">Café Americano 50$</button>
+            <button className="Caffe-milk">Café con leche de almendras 5$</button>
+            <button className="Sandwich">Sandwich de NO jamón y queso vegan 10$</button>
+            <button className="juice">Jugo de frutas natural 7$</button>
+     </div>
+     </>
     )
 }
