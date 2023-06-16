236261804d0dce364f8f6fd6488d72d109ab7716
JackelineGS <jackeline.garcia.serna@gmail.com>
2023-06-16 17:48:52 -0500

    Diseño del login

--- quick-orders/src/app/app.module.ts
+++ quick-orders/src/app/app.module.ts
@@ -2,10 +2,12 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { login } from './components/login/login.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    login
   ],
   imports: [
     BrowserModule
---
+++ quick-orders/src/app/components/login/login.component.ts
@@ -0,0 +1,14 @@
+import { Component } from "@angular/core";
+@Component ({
+    selector: 'loginBurger',
+    templateUrl: './login.component.html',
+    styleUrls: ['./login.component.css']
+})
+export class login {
+    constructor(){
+        console.log("Componente cargado");
+    }
+}
+
+
+
