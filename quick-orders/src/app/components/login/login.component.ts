d64fffc27e748e33a9a5901108bc1a99cd4d07c4
JackelineGS <jackeline.garcia.serna@gmail.com>
2023-06-21 18:15:32 -0500

    Se agregó el código para consultar la API

--- quick-orders/src/app/components/login/login.component.ts
+++ quick-orders/src/app/components/login/login.component.ts
@@ -1,14 +1,32 @@
+import { HttpClient, HttpHeaders } from "@angular/common/http";
 import { Component } from "@angular/core";
-@Component ({
-    selector: 'app-login',
-    templateUrl: './login.component.html',
-    styleUrls: ['./login.component.css']
+
+/*const httpOptions = {
+    headers: new HttpHeaders({
+        'Content-Type': 'application/json',
+        Authorization: 'my-auth-token'
+    })
+};*/
+
+@Component({
+  selector: 'app-login',
+  templateUrl: './login.component.html',
+  styleUrls: ['./login.component.css'],
 })
 export class LoginComponent {
-    constructor(){
-        console.log("Componente cargado");
-    }
+  constructor(private http: HttpClient) {
+    this.getDataFromAPI();
+  }
+
+  getDataFromAPI() {
+    this.http
+      .post('http://localhost:8080/login', {
+        email: 'grace.hopper@systers.xyz',
+        password: '123456',
+      })
+      .subscribe((data: any) => {
+        console.log(data);
+      });
+  }
 }
-
-
 
