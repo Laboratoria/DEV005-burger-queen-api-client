6effc5a5773fc11fffab1ee8b4ec6a441d0ba171
YahairaPerez1994 <yahairaperez2694@gmail.com>
2023-06-23 13:00:42 -0500

    Co-authored-by: Laura Alejandra Cuenca <lauralejandra108@users.noreply.github.com>

--- burgerQeen/src/petitions/products.jsx
+++ burgerQeen/src/petitions/products.jsx
@@ -1,18 +1,20 @@
 /* eslint-disable react/jsx-key */
+import '../waiter/menu.css'
 import { useEffect, useState } from "react";
 import axios from "axios";
 import Button from "../components/buttons";
 import { Menu } from "../waiter/menu";
+//import FilterBtn from "../components/filter-btn";
 
 const Products = () => {
   const [products, setProducts] = useState([]);
+  const [selectedMenu, setSelectedMenu] = useState("");
 
   useEffect(() => {
     const token = localStorage.getItem("accessToken");
-    // console.log("Token de acceso:", token);
+
     axios
       .get("http://localhost:8080/products", {
-        // method: "GET",
         headers: {
           "Content-Type": "application/json",
           Authorization: `Bearer ${token}`,
@@ -26,22 +28,48 @@ const Products = () => {
         console.error(error);
       });
   }, []);
+
+  const handleMenuSelection = (menuType) => {
+    setSelectedMenu(menuType);
+  };
+
+  const filteredProducts = getMenuItems(products, selectedMenu);
+
   return (
     <>
       <div className="container-menu">
+        <h1>Menú</h1>
         <div className="containerBtn">
-          <Button className="btn-desayuno" text="Desayuno" type="submit" />
-          <Button className="btn-almuerzo" text="Almuerzo" type="submit" />
+          <Button
+            className="btn-desayuno"
+            text="Desayuno"
+            type="submit"
+            onClick={() => handleMenuSelection("desayuno")}
+          />
+          <Button
+            className="btn-almuerzo"
+            text="Almuerzo"
+            type="submit"
+            onClick={() => handleMenuSelection("almuerzo")}
+          />
         </div>
         <div className="container-productos">
-          {products.filter((product)=>{
-           return  product.type == "Desayuno"
-          }).map((product) => (
-            <Menu {...product} />
+          {filteredProducts.map((product) => (
+            <Menu key={product.id} {...product} />
           ))}
         </div>
       </div>
     </>
   );
 };
+
+const getMenuItems = (menu, selectedMenu) => {
+  if (selectedMenu === "desayuno") {
+    return menu.filter((product) => product.type === "Desayuno");
+  } else if (selectedMenu === "almuerzo") {
+    return menu.filter((product) => product.type === "Almuerzo");
+  }
+  return [];
+};
+
 export default Products;
