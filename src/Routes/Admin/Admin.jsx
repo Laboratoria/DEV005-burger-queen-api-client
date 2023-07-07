f8eaaa9a89d4db982e58bd30da5328e3f478c1cd
T-Barrios <124935961+T-Barrios@users.noreply.github.com>
2023-07-07 16:17:52 -0400

    creacion tabla de desayuno y almuerzo llamando a la api

--- src/Routes/Admin/Admin.jsx
+++ src/Routes/Admin/Admin.jsx
@@ -2,7 +2,6 @@ import AdminCSS from "../../Style/admin.module.css";
 import Button from "../../components/Button";
 import Header from "../../components/Header";
 
-
 export default function Admin() {
   return <>
     <Header prop="Grace" />
--- src/Routes/Admin/AdminProducts.jsx
+++ src/Routes/Admin/AdminProducts.jsx
@@ -1,9 +1,71 @@
+import React, { useState, useEffect } from 'react';
+import axios from 'axios';
 
+const MenuTable = () => {
+  const [breakfastProducts, setBreakfastProducts] = useState([]);
+  const [lunchProducts, setLunchProducts] = useState([]);
 
-const AdminProducts = () => {
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        const token = localStorage.getItem("token");
+        const response = await axios.get("http://localhost:8080/products", {
+          headers: {
+            "Content-Type": "application/json",
+            Authorization: "Bearer " + token,
+          },
+        });
+        const products = response.data;
+        const breakfastItems = products.filter(
+          (product) => product.type === 'Desayuno'
+        );
+        const lunchItems = products.filter(
+          (product) => product.type === 'Almuerzo'
+        );
+        setBreakfastProducts(breakfastItems);
+        setLunchProducts(lunchItems);
+      } catch (error) {
+        console.error('Error al obtener los datos:', error);
+      }
+    };
+
+    fetchData();
+  }, []);
+
+  const renderTableBody = (products) => {
+    return (
+      <tbody>
+        {products.map((product) => (
+          <tr key={product.id}>
+            <td>{product.name}</td>
+            <td>${product.price}</td>
+          </tr>
+        ))}
+      </tbody>
+    );
+  };
+
   return (
-    <div>AdminProducts</div>
-  )
-}
+    <div>
+      <table>
+        <thead>
+          <tr>
+            <th>Desayuno</th>
+          </tr>
+        </thead>
+        {renderTableBody(breakfastProducts)}
+      </table>
 
-export default AdminProducts
+      <table>
+        <thead>
+          <tr>
+            <th>Almuerzo</th>
+          </tr>
+        </thead>
+        {renderTableBody(lunchProducts)}
+      </table>
+    </div>
+  );
+};
+
+export default MenuTable;
