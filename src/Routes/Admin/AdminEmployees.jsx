1470af0c202822c3849f586b211b4d9d46a1b43b
Carolina Vera <cvera.nahuelcoy@gmail.com>
2023-07-13 13:50:22 -0400

    se crea tabla para agregar usuarios

--- src/Routes/Admin/AdminEmployees.jsx
+++ src/Routes/Admin/AdminEmployees.jsx
@@ -2,52 +2,8 @@ import React from 'react'
 import ApiAdminUser  from '../../Utilities/ApiAdminUser';
 import Header from "../../components/Header";
 import TopBarAdmin from '../../components/topBarAdmin';
-import { useState } from 'react';
 
-const AdminEmployees = ({ products, type }) => {
-  const [editableProducts, setEditableProducts] = useState(products);
-
-  const handleEdit = (productId, fieldName, newValue) => {
-    const updatedProducts = editableProducts.map((product) => {
-      if (product.id === productId) {
-        return {
-          ...product,
-          [fieldName]: newValue,
-        };
-      }
-      return product;
-    });
-    setEditableProducts(updatedProducts);
-  };
-
-  const handleUpdate = () => {
-    // Perform the update logic here using the updated products
-    // For example, you can make an API call to update the table data
-    console.log('Updated products:', editableProducts);
-  };
-
-  const renderTableRows = (data) => {
-    return data.map((product) => (
-      <tr key={product.id}>
-        <td>
-          <input
-            type="text"
-            value={product.name}
-            onChange={(e) => handleEdit(product.id, 'name', e.target.value)}
-          />
-        </td>
-        <td>
-          <input
-            type="text"
-            value={product.price}
-            onChange={(e) => handleEdit(product.id, 'price', e.target.value)}
-          />
-        </td>
-      </tr>
-    ));
-  };
-
-export const AdminEmployees = () => {
+const AdminEmployees = () => {
   return (
     <>
 
@@ -56,23 +12,6 @@ export const AdminEmployees = () => {
      <TopBarAdmin/>
      <ApiAdminUser refresh={refresh} />
       <CreateUserTable onUserCreated={handleUserCreated} />
-    </>
-  )
-}
-      {type === 'desayunoEdit' && (
-        <tbody>
-          {renderTableRows(editableProducts)}
-        </tbody>
-      )}
-      {type === 'desayunoEdit' && (
-        <tfoot>
-          <tr>
-            <td colSpan="2">
-              <button onClick={handleUpdate}>Update</button>
-            </td>
-          </tr>
-        </tfoot>
-      )}
     </>
   );
 };
