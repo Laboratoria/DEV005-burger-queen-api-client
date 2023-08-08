d9f597485b6286c843f76f9e31456d54907bdc94
_AngieMora1 <angiemorarosas@gmail.com>
2023-08-08 09:53:07 -0500

    peticiones get

--- bqac/src/App.jsx
+++ bqac/src/App.jsx
@@ -1,35 +1,54 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import bearerToken from "./http";
+import axios from "axios";
 
 function App() {
-  const [count, setCount] = useState(0)
+  const headers = { Authorization: `Bearer ${bearerToken.token}` };
+
+  const getTrending = () => {
+    axios({
+      method: "get",
+      url: "http://localhost:8080/products",
+      headers,
+    }).then((response) => console.log("get products", response));
+    axios({
+      method: "get",
+      url: "http://localhost:8080/users",
+      headers,
+    }).then((response) => console.log("get users", response));
+    axios({
+      method: "get",
+      url: "http://localhost:8080/orders",
+      headers,
+    }).then((response) => console.log("get orders", response));
+    const loginDta = axios.post(
+        "http://localhost:8080/login",
+        {
+          email: "grace.hopper@systers.xyz",
+          password: "123456",
+        },
+        headers
+      )
+      .then((response) => {
+        const datas = response; 
+        console.log(datas.data.accessToken)});
+      axios.patch(
+        " http://localhost:8080/orders/2",
+        {
+          status: "delivered",
+          dateProcessed: "2022-03-05 16:00"
+      },
+      loginDta
+      )
+      .then((response) => console.log("patch orders", response));
+  };
+
+  getTrending();
 
   return (
     <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      <h1>Burger QAC</h1>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
---
+++ bqac/src/http.js
@@ -0,0 +1,6 @@
+
+const bearerToken = {
+    token:"eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJlbWFpbCI6ImdyYWNlLmhvcHBlckBzeXN0ZXJzLnh5eiIsImlhdCI6MTY5MTUwMzc0NywiZXhwIjoxNjkxNTA3MzQ3LCJzdWIiOiIyIn0.Lfj7OzvSAUnj_qpxZaW8ka7ijN4mbMTz3Fb0I8kOYhw"
+}
+
+export default bearerToken
