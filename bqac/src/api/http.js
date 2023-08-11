243432a5a11ef90a6b3771083f9a1656943c0f59
Karen Huamán <karmahucents@gmail.com>
2023-08-11 10:26:55 -0500

    add: añadiendo fuente e icono

--- bqac/src/api/http.js
+++ bqac/src/api/http.js
@@ -1,7 +1,7 @@
 
 const apiConfig = {
     baseUrl: 'http://localhost:8080/',
-    token:"eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJlbWFpbCI6ImdyYWNlLmhvcHBlckBzeXN0ZXJzLnh5eiIsImlhdCI6MTY5MTU5MjA0MCwiZXhwIjoxNjkxNTk1NjQwLCJzdWIiOiIyIn0.LnM-DILlzIYF7jz-jDoYgJEaSaNPeCAGWZL7GOORycQ"
+    token:"eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJlbWFpbCI6ImdyYWNlLmhvcHBlckBzeXN0ZXJzLnh5eiIsImlhdCI6MTY5MTY4MTczMSwiZXhwIjoxNjkxNjg1MzMxLCJzdWIiOiIyIn0.MK1gAvU15vt1L0HxCHBIbG9uQtFouFS8fAf2kcR60bI"
 }
 
 export default apiConfig;
