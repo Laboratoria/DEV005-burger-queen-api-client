c9ae861b19bbbba6630f6a502bc6d555ae9d3a95
_AngieMora1 <angiemorarosas@gmail.com>
2023-08-11 10:35:41 -0500

    trabajo creacion de usuarios

--- bqac/src/api/http.js
+++ bqac/src/api/http.js
@@ -1,7 +1,7 @@
 
 const apiConfig = {
     baseUrl: 'http://localhost:8080/',
-    token:"eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJlbWFpbCI6ImdyYWNlLmhvcHBlckBzeXN0ZXJzLnh5eiIsImlhdCI6MTY5MTU5MjA0MCwiZXhwIjoxNjkxNTk1NjQwLCJzdWIiOiIyIn0.LnM-DILlzIYF7jz-jDoYgJEaSaNPeCAGWZL7GOORycQ"
+    token:"eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJlbWFpbCI6ImdyYWNlLmhvcHBlckBzeXN0ZXJzLnh5eiIsImlhdCI6MTY5MTc2Mzg2NCwiZXhwIjoxNjkxNzY3NDY0LCJzdWIiOiIyIn0.JqDk3A3Urj7xTCNcLoRcJxuVs85a9DlvMcDeTNWbO4g"
 }
 
 export default apiConfig;
