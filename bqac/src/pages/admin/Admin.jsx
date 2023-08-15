9c0224e55a8885a494e02a8c544be010d32ed7f0
Karen Huamán <karmahucents@gmail.com>
2023-08-15 10:51:52 -0500

    add: añadiendo navbar y table con boostrap

--- bqac/src/pages/admin/Admin.jsx
+++ bqac/src/pages/admin/Admin.jsx
@@ -1,35 +1,69 @@
-import { Navbar, Nav, Container, Button } from 'react-bootstrap';
+import { Navbar, Nav, Container, Button, Table } from "react-bootstrap";
 import { PiHamburgerBold } from "react-icons/pi";
-import { BiDoorOpen } from "react-icons/bi";
-import './admin.css'
+import { RxExit } from "react-icons/rx";
+import { BsTrash, BsPencilSquare } from "react-icons/bs";
+import "./admin.css";
 
 export const Admin = () => {
   return (
-    <Container className='bg-admin'>
-      <Navbar className='navbar'>
-      <Container className='cont-logo'>
-        <Container className="">
-          <Navbar.Brand >
-            <PiHamburgerBold
-              className=""
-            />{' '}
-            BQAC Team
+    <Container className="bg-admin">
+      <Navbar>
+        <Container className="header">
+          <Navbar.Brand className="logo">
+            <PiHamburgerBold className="burger" />
+            <Navbar.Text>BQAC Team</Navbar.Text>
           </Navbar.Brand>
+
+          <Nav className="nav-links">
+            <Nav.Link href="#" className="nav-opt">
+              Usuarios
+            </Nav.Link>
+            <Nav.Link href="#" className="nav-opt">
+              Productos
+            </Nav.Link>
+          </Nav>
+
+          <Button className="btn-close">
+            <RxExit />
+          </Button>
         </Container>
+      </Navbar>
 
-
-        <Nav variant='tabs' className='cont-items'>
-        <Nav.Item>
-          <Nav.Link href="#">Active</Nav.Link>
-        </Nav.Item>
-        <Nav.Item>
-          <Nav.Link eventKey="link-1">Link</Nav.Link>
-        </Nav.Item>
-        </Nav>
-
-          <Button className='cont-btn' variant="primary"><BiDoorOpen/></Button>
+      <Container className="crud-items">
+      <Table striped="columns">
+          <thead>
+            <tr>
+              <th>Id</th>
+              <th>Correo</th>
+              <th>Contraseña</th>
+              <th>Rol</th>
+              <th>Opciones</th>
+            </tr>
+          </thead>
+          <tbody>
+            <tr>
+              <td>1</td>
+              <td>Mark</td>
+              <td>Otto</td>
+              <td>admin</td>
+              <td>
+                <BsPencilSquare />
+                <BsTrash />
+              </td>
+            </tr>
+            <tr>
+              <td>2</td>
+              <td>Jacob</td>
+              <td>Thornton</td>
+              <td>admin</td>
+              <td>
+                <BsPencilSquare />
+                <BsTrash />
+              </td>
+            </tr>
+          </tbody>
+        </Table>
       </Container>
-    </Navbar>
     </Container>
-  )
-}
+  );
+};
