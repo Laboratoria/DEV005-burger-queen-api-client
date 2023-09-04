024fe45d4c0c13c6db85dbdd449caa40c86f597f
Ana Laura Vargas Piña <analauvapi15@gmail.com>
2023-09-04 08:47:33 -0600

    agrega productos

--- src/components/admin/ProductsManagement.jsx
+++ src/components/admin/ProductsManagement.jsx
@@ -5,163 +5,155 @@ import Products from "../Products/Products";
 import { createProduct, getproduct } from "../../services/UseAxios";
 import Navigation from "../navigation/navigation";
 import Buttons from "../Buttons/Buttons";
-import Swal from 'sweetalert2';
+import Swal from "sweetalert2";
 
 function ProductsManagement() {
-// CREAR PRODUCTOS
+
+// CREAR PRODUCTOS-----------------------------------------------------------
   const handleSubmit = async (event) => {
-      event.preventDefault();
- 
-      if (!nameProduct || !priceProduct) {
-        Swal.fire({
-          title: 'Please fill in the required information',
-          icon: 'error',
-          confirmButtonColor: '#D62828',
-        });
-        return;
-      } 
+    event.preventDefault();
 
+    if (!nameProduct || !priceProduct) {
+      Swal.fire({
+        title: "Please fill in the required information",
+        icon: "error",
+        confirmButtonColor: "#D62828",
+      });
+      return;
+    }
+    try {
+      const response = await createProduct(
+        nameProduct,
+        priceProduct,
+        image,
+        typeProduct
+      );
+
+    // Agregar el nuevo producto a la lista existente
+    setProducts([...products, response]);
+
+      Swal.fire({
+        icon: 'success',
+        title: 'Product created succesfully',
+        showConfirmButton: false,
+        timer: 1500
+      });
+
+    // Restaurar los valores de los campos después de agregar el producto
+      setNameProduct("");  
+      setPriceProduct(""); 
+      setImage(""); 
+
+    } catch (error) {
+      Swal.fire({
+        title: "Error registering product, please try again later",
+        icon: "error",
+        showConfirmButton: false,
+        timer: 1500
+      });
+    }
+  };
+
+//BARRA DE NAVEGACION ----------------------------------------------
+  const [selectedTab, setSelectedTab] = useState("All");
+
+  const handleTabChange = (tab) => {
+    setSelectedTab(tab);
+  };
+
+// MOSTRAR PRODUCTOS-----------------------------------------------
+
+  const [products, setProducts] = useState([]);
+
+  useEffect(() => {
+    const fetchProducts = async () => {
       try {
-        const response = await createProduct(nameProduct, priceProduct, image, typeProduct);
-        console.log(response, 'tu te me quitas')
-        nameProduct("");
-        priceProduct("");
-        image("");
-        typeProduct("");
-        return response
-       
+        const response = await getproduct();
+        //console.log(response, "PRODUCTOS EN ADMIN");
+        setProducts(response);
       } catch (error) {
-        Swal.fire({
-          title: 'Error registering product, please try again later',
-          icon: 'error',
-          confirmButtonColor: '#D62828',
-        });
+        console.error("Error fetching products:", error);
       }
-       
-      
-    } 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-    const [selectedTab, setSelectedTab] = useState('Breakfast');
-
-    const handleTabChange = (tab) => {
-      setSelectedTab(tab);
     };
+    fetchProducts();
+  }, []);
 
-    const [products, setProducts] = useState([]);
+  // ESTADO DE INPUT DE IMAGEN------------------------------
+  const [image, setImage] = useState("");
 
-    useEffect(() => {
-      const fetchProducts = async () => {
-        try {
-          const response = await getproduct();
-          console.log(response, 'si yo fuera tu');    
-          setProducts(response);
-        } catch (error) {
-          console.error("Error fetching products:", error);
-        }
-      }; 
-      fetchProducts();
-    }, []);
+  const handleImageChange = (event) => {
+    console.log(event.target.value);
+    setImage(event.target.value);
+  };
+  // ESTADO DE INPUT DE NOMBRE DE PRODUCTO---------------------------
+  const [nameProduct, setNameProduct] = useState("");
 
-// ESTADO DE INPUT DE IMAGEN
-const [image, setImage] = useState("");
+  const handleNameProductChange = (event) => {
+    console.log(event.target.value);
+    setNameProduct(event.target.value);
+  };
+  // ESTADO DE INPUT DE PRECIO-----------------------------------------
+  const [priceProduct, setPriceProduct] = useState("");
 
-    const handleImageChange = (event) => {
-      console.log(event.target.value);
-      setImage(event.target.value);
-    };
-// ESTADO DE INPUT DE NOMBRE DE PRODUCTO
-const [nameProduct, setNameProduct] = useState("");
+  const handlesetPriceProductChange = (event) => {
+    console.log(event.target.value);
+    setPriceProduct(event.target.value);
+  };
 
-    const handleNameProductChange = (event) => {
-      console.log(event.target.value);
-      setNameProduct(event.target.value);
-    };
-// ESTADO DE INPUT DE PRECIO
-const [priceProduct, setPriceProduct] = useState("");
+  // ESTADO DE TIPO DE COMIDA------------------------------------------
+  const [typeProduct, setTypeProduct] = useState("");
+  function handleTypeProductChange(e) {
+    console.log(e.target.value, "SELECTED TAB");
+    setTypeProduct(e.target.value);
+  }
 
-    const handlesetPriceProductChange = (event) => {
-      console.log(event.target.value);
-      setPriceProduct(event.target.value);
-    };
-
-// ESTADO DE TIPO DE COMIDA
-    const [typeProduct, setTypeProduct] = useState('')
-    function handleTypeProductChange(e) {
-        console.log(e.target.value, "lililili");
-        setTypeProduct(e.target.value);
-      }
-    
-      const items = [
-        {
-          id: 1,
-          value: "Breakfast",
-        },
-        {
-          id: 2,
-          value: "Lunch",
-        },
-       
-      ];
-
+  const items = [
+    {
+      id: 1,
+      value: "Breakfast",
+    },
+    {
+      id: 2,
+      value: "Lunch",
+    },
+  ];
 
   return (
     <>
       <section>
         <form action="" onSubmit={handleSubmit}>
-        <input 
-        className="inputs-login"
-        type="text" 
-        name="nameProduct" 
-        id="" 
-        placeholder=" Product Name" 
-        value={nameProduct}
-        onChange={handleNameProductChange}/>
-        <input 
-        
-        type="url" 
-        name="url" 
-        id="" 
-        onChange={handleImageChange}
-        
-        />
-        <input
-        onChange={handlesetPriceProductChange} className="inputs-login" type="number"
-        name="priceProduct" 
-        placeholder=" $ Product Price" 
-        value={priceProduct} />
-         
-          <Dropdown items={items} handleOnChange={handleTypeProductChange}/>
-          
-          <Buttons
-          tag="Upload" 
-          type="submit"/>
+          <input
+            className="inputs-login"
+            type="text"
+            name="nameProduct"
+            id=""
+            placeholder=" Product Name"
+            value={nameProduct}
+            onChange={handleNameProductChange}
+          />
+          <input type="url" name="url" id="" onChange={handleImageChange} />
+          <input
+            onChange={handlesetPriceProductChange}
+            className="inputs-login"
+            type="number"
+            name="priceProduct"
+            placeholder=" $ Product Price"
+            value={priceProduct}
+          />
 
+          <Dropdown items={items} handleOnChange={handleTypeProductChange} />
+
+          <Buttons tag="Upload" type="submit" />
         </form>
       </section>
       <section>
-
-      <Navigation
-          tabs={["All", "Breakfast", "Lunch" ]}
+        <Navigation
+          tabs={["All", "Breakfast", "Lunch"]}
           activeTab={selectedTab}
           onSelectTab={handleTabChange}
         />
-        <Products productType={selectedTab}
+        <Products
+          productType={selectedTab === "All" ? "" : selectedTab}
           products={products}
         />
       </section>
--- src/services/UseAxios.jsx
+++ src/services/UseAxios.jsx
@@ -156,7 +156,7 @@ console.log(response.data, 'popoppopopopoppp')
    
   }
 }
-// CREAR PRODUCTO
+// CREAR PRODUCTO----------------------------------------------------------
 export async function createProduct(nameProduct, price, image, type ) {
  
   try {
@@ -175,10 +175,10 @@ export async function createProduct(nameProduct, price, image, type ) {
   Authorization: `${authToken}`,
 },})
 
-console.log(response.data, 'OMG')
+console.log(response.data, 'OMG CREADO CORRECTAMENTE  :D ')
     return response.data;
   } catch (error) {
-    console.log(error.response.data, 'clicli')
+    console.log(error.response.data, 'NO FUNCIONO :(')
    
   }
 }
